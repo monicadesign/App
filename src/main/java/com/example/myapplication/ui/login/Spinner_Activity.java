b80aac918f90a83415bf4eca98730d1a1a02c376
Monica <vieiramonica71@gmail.com>
2020-05-30 00:54:41 +0100

    spinner array

--- src/main/java/com/example/myapplication/ui/login/Spinner_Activity.java
+++ src/main/java/com/example/myapplication/ui/login/Spinner_Activity.java
@@ -3,6 +3,7 @@ package com.example.myapplication.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,8 +19,8 @@ public class Spinner_Activity extends AppCompatActivity implements AdapterView.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registo);
 
-        Spinner spinner = findViewById(R.id.spinner1);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Distrito,android.R.layout.simple_spinner_item);
+        Spinner spinner = findViewById(R.id.aSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Distritos,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
@@ -30,6 +31,10 @@ public class Spinner_Activity extends AppCompatActivity implements AdapterView.O
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String text = parent.getItemAtPosition(position).toString();
         Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
+        if (position == 0) {
+            Toast.makeText(getApplicationContext(),"Este é o item nº0",Toast.LENGTH_LONG).show();
+        }
+        Log.d("teste","a testar " + position + " "+ id + " " + view);
     }
 
     @Override
