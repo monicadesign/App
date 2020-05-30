47bf4a6071b8cf7368a5317a66eec71299a3e279
Monica <vieiramonica71@gmail.com>
2020-05-30 01:21:54 +0100

    spinner

--- src/main/java/com/example/myapplication/Register.java
+++ src/main/java/com/example/myapplication/Register.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Register extends AppCompatActivity implements View.OnClickListener {
+public class Register extends AppCompatActivity implements View.OnClickListener , AdapterView.OnItemSelectedListener {
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,13 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
         btn_voltar.setOnClickListener(this);
         btn_go_to_login.setOnClickListener(this);
 
+        //Area Spinner
+        Spinner spinner = findViewById(R.id.aSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Distritos,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
     }
     @Override
     public void onClick(View view_clicada) {
@@ -31,5 +43,22 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
         if (view_clicada == findViewById(R.id.btn_go_to_sobre)){
             this.startActivity(new Intent(this,LogInActivity.class));
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
+        if (position == 0) {
+            Toast.makeText(getApplicationContext(),"Este é o item nº0",Toast.LENGTH_LONG).show();
+            ((TextView)parent.getChildAt(0)).setTextColor(getColor(R.color.appcolor3));
+        }
+
+        parent.getChildAt(0).setFocusable(false);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
